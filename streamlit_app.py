f567f7663b9c7d4a7f69ae460fdb100e0af9460d
Moris-Polanco <116950929+Moris-Polanco@users.noreply.github.com>
2023-01-20 16:43:55 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,6 @@
+import speech_recognition as sr
 import streamlit as st
-import openai_secret_manager
-import openai
-import base64
-from pydub import AudioSegment
-import os
 
-# Autenticación de OpenAI (oculta la clave en una variable de entorno)
-openai.api_key = os.environ.get("OPENAI_API_KEY")
-
-# Configurar el modelo GPT-3 para usar el idioma español
-model_engine = "text-davinci-002"
-prompt = (f"transcribir notas de audio en español")
-
-# Crear la interfaz de usuario
 st.set_page_config(page_title="Transcripción de audio en español", page_icon=":microphone:", layout="wide")
 st.title("Transcripción de audio en español")
 
@@ -20,22 +8,19 @@ audio_file = st.file_uploader("Selecciona un archivo de audio", type=["mp3","wav
 
 if audio_file is not None:
     audio_bytes = audio_file.read()
-    with open("audio.mp3", "wb") as f:
+    with open("audio.wav", "wb") as f:
         f.write(audio_bytes)
-    sound = AudioSegment.from_file("audio.mp3", format="mp3")
-    sound.export("audio.wav", format="wav")
-    with open("audio.wav", "rb") as f:
-        audio_bytes = f.read()
-        audio_base64 = base64.b64encode(audio_bytes).decode()
-    response = openai.Completion.create(
-        engine=model_engine,
-        prompt=prompt,
-        audio=audio_base64,
-        temperature=0.5,
-    )
-    result = response["choices"][0]["text"].strip()
-    st.success("Transcripción completada")
-    st.write("Transcripción:")
-    st.write("```" + result + "```")
+    r = sr.Recognizer()
+    with sr.AudioFile("audio.wav") as source:
+        audio = r.record(source)
+    try:
+        text = r.recognize_google(audio, language='es-ES')
+        st.success("Transcripción completada")
+        st.write("Transcripción:")
+        st.write("```" + text + "```")
+    except sr.UnknownValueError:
+        st.error("Lo siento, no he podido entender el audio")
+    except sr.RequestError as e:
+        st.error("Error en el servicio; {0}".format(e))
 else:
     st.write("Por favor, seleccione un archivo de audio.")
